98bfa4147fdb9c8995f6247feaee7e5c6d3e30a0
TobiasTrempeck <tobias.trempeck@capgemini.com>
2021-04-16 16:43:22 +0200

    removed tsx files

--- fantasy_bottom.tsx
+++
@@ -1,9 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.4" tiledversion="1.4.3" name="fantasy_bottom" tilewidth="32" tileheight="32" tilecount="208" columns="16">
- <image source="tilesets/fantasy_bottom.png" width="512" height="416"/>
- <tile id="47">
-  <properties>
-   <property name="collides" type="bool" value="true"/>
-  </properties>
- </tile>
-</tileset>
