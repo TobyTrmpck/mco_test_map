cc6df2f46d0777a508b87ea39ee8055b71fc7f1c
TobiasTrempeck <tobias.trempeck@capgemini.com>
2021-04-16 16:53:21 +0200

    removed last tsx file

--- OL_Innenraum_HGSS_Newtiteuf.tsx
+++
@@ -1,9 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.4" tiledversion="1.4.3" name="OL_Innenraum_HGSS_Newtiteuf" tilewidth="32" tileheight="32" tilecount="496" columns="16">
- <image source="tilesets/OL_Innenraum_HGSS_Newtiteuf.png" width="533" height="1002"/>
- <tile id="72">
-  <properties>
-   <property name="collides" type="bool" value="true"/>
-  </properties>
- </tile>
-</tileset>
